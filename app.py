38d513e3e7212988776b62755a76e82ba95b3138
aimer999 <zhoujiuzhou99@gmail.com>
2024-11-13 03:50:17 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return "Hello, Flask!"
+
+# 启动服务器
+if __name__ == '__main__':
+    app.run(debug=True)
