ae9323355cbd8628250c8408c4c04f83870a6f39
aimer999 <zhoujiuzhou99@gmail.com>
2024-12-04 14:52:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flash import render_template,request
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def home():
-    return "Hello, Flask!"
+def index():
+    return(render_template("index.html"))
 
-# 启动服务器
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
